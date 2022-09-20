22f3a2484845bbe905fcd7c7d1a07af4840f6989
adambala <99123480+adambala@users.noreply.github.com>
2022-09-21 00:22:03 +0300

    Added input implementation by additional integer check

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
 from random import randint
-
 from gcd import gcd
+
+
+def intInput(inputText='') -> int:
+    """
+    Uses built-in input() function and return an int value of input string.
+
+    If input string cannot be turned into an integer the function will ask user to write it again.
+
+    Parameters:
+        inputText: String used for input() function
+    """
+    while True:
+        try:
+            returnValue = int(input(inputText))
+        except ValueError:
+            print("Input must be an integer value!")
+            continue
+
+        return returnValue
 
 
 def testGCD(rangeBoundary, testNumber):
@@ -9,13 +27,13 @@ def testGCD(rangeBoundary, testNumber):
 
     Prints the result to the console.
 
-    Optional keyword arguments:
-    - rangeBoundary: maximum possible generated integer
-    - testNumber:    the number of tests to be calculated.
+    Parameters:
+        rangeBoundary: maximum possible generated integer
+        testNumber:    the number of tests to be calculated.
     """
     for i in range(testNumber):
-        m = randint(0, rangeBoundary)
-        n = randint(0, rangeBoundary)
+        m = randint(0, abs(rangeBoundary))  # abs() used to avoid errors with negative numbers
+        n = randint(0, abs(rangeBoundary))
         result = gcd(m, n)
 
         # output
@@ -26,6 +44,6 @@ def testGCD(rangeBoundary, testNumber):
 
 
 if __name__ == "__main__":
-    rB = int(input("Maximum possible integer: "))
-    tN = int(input("Number of tests: "))
+    rB = intInput("Maximum possible integer: ")
+    tN = intInput("Number of tests: ")
     testGCD(rangeBoundary=rB, testNumber=tN)
